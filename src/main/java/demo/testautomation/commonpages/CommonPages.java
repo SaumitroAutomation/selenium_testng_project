790e079e14c9f0bdbe6f2cd6c41db8a996195302
SaumitroAutomation <autocloud55@gamil.com>
2024-05-07 23:44:37 +0530

    commonpages.java

---
+++ src/main/java/demo/testautomation/commonpages/CommonPages.java
@@ -0,0 +1,15 @@
+package demo.testautomation.commonpages;
+
+public class CommonPages {
+	
+	public void login(String steps) {
+		try {
+			
+		}
+		catch(Exception e) {
+			System.out.println("Error at "+ steps+ e.getMessage());
+			
+		}
+	}
+
+}
